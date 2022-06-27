bc1be693a60a3bf7400b5bab11f653ea6e7bde8c
Anastacio <gl0inn@hotmail.com>
2022-06-27 14:28:36 -0300

    First commit

--- scripts.js
+++ scripts.js
@@ -1,51 +1,55 @@
-const display = document.getElementById('display')
-let expression = []
-const historico = document.getElementById('historico')
+const display1 = document.getElementById('display1')
+const display2 = document.getElementById('display2')
+let expression = ''
+let lastOperator;
 const buttons = document.querySelectorAll('button')
 
 buttons.forEach(button => button.addEventListener('click', () => {
     if(isNaN(button.id) & button.id != '='){
-        if(display.textContent != ''){expression.push(display.textContent)}
-        display.textContent = ''
-        expression.push(button.id)
-        console.log(expression)
-        historico.textContent = operate(expression)
+        lastOperator = button.id
+        display1.textContent = ''
+        display2.textContent = operate(button.id)
+        expression = '' 
     }
     if(!isNaN(button.id)){
-        display.textContent += button.id
+        if(display1.textContent === '0'){display1.textContent = ''}
+        expression += button.id
+        display1.textContent += button.id
     }
-    if (button.id === '='){
-        expression.push(display.textContent)
-        display.textContent = ''
-        historico.textContent = operate(expression)
+    if(button.id ==='='){
+        display1.textContent=''
+        display2.textContent = operate(button.id)
+        expression = ''   
     }
-    
 }))
 
-function operate(array){
-    let operators = []
-    let numbers = []
-    for (let values of array){
-        if (isNaN(values)){operators.push(values)}
-        else {numbers.push(values)}
+let firstValue = 0
+function operate(operator){
+    switch(operator) {
+        case '+':
+            firstValue += Number(expression)
+            break;
+        case '-': 
+            firstValue -= Number(expression)
+            break;
+        case '/':
+                if (Number(expression) === '') {firstValue = 0}
+                else {firstValue /= Number(expression)}
+            break;
+        case '*':
+            firstValue *= Number(expression)
+            break;
+        case '=':
+            if(lastOperator ==='+'){firstValue += Number(expression)}
+            else if(lastOperator ==='-'){firstValue -= Number(expression)}
+            else if (lastOperator === '/'){
+                if (Number(expression) === 0){firstValue = 'Error'}
+                else{firstValue /= Number(expression)}
+            }
     }
-    if (operators.length != numbers.length){operators.splice(0,0,'+')}
-    console.log(operators, numbers)
-    let inicial = 0
-    let i = 0
-    let result = numbers.reduce((inicial, value) => {
-        if(operators[i] === '+'){inicial += Number(value)}
-        else if (operators[i] === '-'){inicial -= Number(value)}
-        else if (operators[i] === '*'){inicial *= Number(value)}
-        else if (operators[i] === '/'){
-            if (Number(value) === 0){ inicial= 'You should not do this.'}
-            else {inicial /= Number(value)}
-        }
-        console.log(i)
-        i++
-        return inicial
-    }, inicial)
-    console.log(result)
-    return result
+    return firstValue
 }
+
+
+
 
