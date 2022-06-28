db249c2daf328d42adbef11df60614294d06c264
Anastacio <gl0inn@hotmail.com>
2022-06-28 10:13:34 -0300

    Better logic, Added CSS

--- scripts.js
+++ scripts.js
@@ -1,32 +1,33 @@
 const display1 = document.getElementById('display1')
 const display2 = document.getElementById('display2')
+const buttons = document.querySelectorAll('button')
 let firstValue = 0
 let expression = [firstValue]
-const buttons = document.querySelectorAll('button')
 
 buttons.forEach(button => button.addEventListener('click', () => {
-    if(button.id ==='='){
+    if(button.id ==='equal'){
         display1.textContent = operate(expression[1]) 
-
-    }
-
-    if(!isNaN(button.id)){
+        showDisplay2(button)
+    };
+    if(!isNaN(button.id) || button.id === '.'){
         lastNumber = Number(button.id)
         if (expression.length===1){expression.push('+')}
         if (expression[2]===undefined){expression.push(button.id)}
         else (expression[2] += button.id) 
-        display2.textContent += button.id
-        
-    }
-
-    if(isNaN(button.id) & button.id != '='){
+        showDisplay2(button)
         console.log(expression)
+    };
+    if(isNaN(button.id) & button.id != 'equal' & button.id != '.'){
         display1.textContent = operate(expression[1])
         lastOperator = button.id
         expression.push(button.id)
-        display2.textContent += button.id
-    }
-}))
+        showDisplay2(button)
+    };
+    if (button.id === 'clearAll'){
+        clearAll()
+    };
+    lowerCaseDisplay1();
+}));
 
 
 function operate(operator){
@@ -50,9 +51,34 @@ function operate(operator){
             break;
     }
     if(expression.length===3){expression = [firstValue]}
-    return firstValue
-}
+    return Number(firstValue.toFixed(6))
+};
 
 
+function clearAll(){
+    display1.textContent = 0 
+    display2.textContent = 0 
+    firstValue = 0
+    expression = [firstValue]
+};
 
+
+function showDisplay2(button){
+    if(isNaN(button.id)){
+        if(display2.textContent==='0'){display2.textContent = button.id}
+        else if (isNaN(display2.textContent[display2.textContent.length-1])){}
+        else {display2.textContent += button.id}   
+    }
+    else{
+        if(display2.textContent==='0'){display2.textContent = button.id}
+        else{display2.textContent += button.id}
+    }
+    if(button.id === 'equal'){display2.textContent = display1.textContent}
+};
+
+function lowerCaseDisplay1(){
+    if(display1.textContent.length>16){
+        display1.style.fontSize = '35px'
+    }
+};
 
