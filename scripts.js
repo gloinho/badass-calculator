08a3ed32f0bbe7d2100fb7e536041e61c1be859e
Anastacio <gl0inn@hotmail.com>
2022-06-28 15:44:58 -0300

    Testing new logic

--- scripts.js
+++ scripts.js
@@ -2,37 +2,34 @@ const display1 = document.getElementById('display1')
 const display2 = document.getElementById('display2')
 const buttons = document.querySelectorAll('button')
 let firstValue = 0
-let expression = ['']
+let lastNumber = ''
+let expression = ['+']
 
 buttons.forEach(button => button.addEventListener('click', () => {
-    button.classList.add('transition')
-    if (firstValue === 'You can\'t divide by zero'){clearAll()}
+    button.classList.add('transition') //Button animation.
+
+    if (firstValue === 'You can\'t divide by zero'){clearAll()} 
+
     if(button.id ==='equal'){
-        expression.push(lastOperator)
-
-
-      
-
+        operate()
+        showDisplay2(button)
     };
-    if(!isNaN(button.id) || button.id === '.'){
-        lastNumber = Number(button.id)
-        if(expression[0] === undefined){expression[0] = button.id}
-        else (expression[0] += button.id)
-        console.log(expression)
 
-
+    if(!isNaN(button.id)){
+        if(expression[1] === undefined){expression[1] = button.id}
+        else (expression[1] += button.id)
+        lastNumber = expression[1]
+        showDisplay2(button)
     };
+
     if(isNaN(button.id) & button.id != 'equal' & button.id != '.'){
-        if(expression.length === 2){display1.textContent = operate ()}
+        operate()
         lastOperator = button.id
         expression.push(button.id)
+        showDisplay2(button)
         console.log(expression)
-        
-        
-
     };
-    if(expression.length === 2){display1.textContent = operate ()}
-    console.log(expression)
+ 
     
     if (button.id === 'clearAll'){
         clearAll()
@@ -43,30 +40,36 @@ buttons.forEach(button => button.addEventListener('click', () => {
 buttons.forEach(button => button.addEventListener('transitionend', removeTransition))
 
 function operate(){
-    if(!isNaN(expression[1])){expression.reverse()}
-    if(expression[0] === '+' & expression[1] === '+'){firstValue += lastNumber}
-    else if (expression[0] === '-' & expression[1] ==='+' || expression[0] === '+' & expression[1] ==='-' ){firstValue = firstValue*(-1)}
-    else if (expression[0] === '-' & expression[1] === '-'){firstValue -= lastNumber}
-    else{
-        switch(expression[1]) {
-            case '+':
-                firstValue += Number(expression[0])
-                break;
-            case '-':
-                firstValue -= Number(expression[0])
-
-                break;
-            case '/':
-
-                break;
-            case '*':
-
-                break;
+    if (expression.length===2){
+        if(!isNaN(expression[1])){expression.reverse()} // Ajusting expression to be in the [signal, number] format.
+        // Repeated Signals:
+        if(expression[0] === '+' & expression[1] === '+'){firstValue += Number(lastNumber)}
+        else if (expression[0] === '-' & expression[1] === '-'){firstValue -= Number(lastNumber)}
+        else if (expression[0] === '/' & expression[1] === '/'){firstValue /= Number(lastNumber)}
+        else if (expression[0] === '*' & expression[1] === '*'){firstValue *= Number(lastNumber)}
+        else if (expression[0] === '-' & expression[1] ==='+' || expression[0] === '+' & expression[1] ==='-' ){firstValue = firstValue*(-1)}
+        else{
+            switch(expression[1]) {
+                case '+':
+                    firstValue += Number(expression[0])
+                    break;
+                case '-':
+                    firstValue -= Number(expression[0])
+                    break;
+                case '/':
+                    firstValue /= Number(expression[0])
+                    if(firstValue === Infinity){firstValue = 'You cannot divide by zero...'}
+                    break;
+                case '*':
+                    firstValue *= Number(expression[0])
+                    break;
+                default:
+                    firstValue = 'error'
+            }
         }
-    }
-
-    if(expression.length===2){expression = ['']}
-    return firstValue 
+    display1.textContent = firstValue
+    expression = []
+    };
 };
 
 
@@ -74,7 +77,7 @@ function clearAll(){
     display1.textContent = 0 
     display2.textContent = 0 
     firstValue = 0
-    expression = [firstValue]
+    expression = []
 };
 
 
